d32e070e77f6911de238c14c31d220d12b1222fd
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-02-17 15:42:18 -0600

    Add javascript functionality

---
+++ main.js
@@ -0,0 +1,21 @@
+var button = document.getElementsByTagName ('button')[0]
+
+var ourRandomRestaurantItem = document.createElement('p');
+var restaurantItemWrapper = document.getElementById('generator-resutls');
+restaurantItemWrapper.appendChild(ourRandomRestaurantItem);
+
+var placeholderText = document.createTextNode('');
+ourRandomRestaurantItem.appendChild(placeholderText);
+
+var restaurantItems = ['Hivolt', 'Steins Market and Deli', 'District Donuts', 'Turkey and the Wolf', 'Saba', 'La Boulangerie', 'Cochon', 'Pizza Domenica', 'Mais Arepas', 'Parkway Poboys' , 'Dat Dog', 'Peche']
+
+var myFunction = function() {
+  restaurantItems.sort(function(a, b){return 0.5 - Math.random()});
+  var ourRandomRestaurantItemFiller = document.createTextNode(restaurantItems[0]);
+  ourRandomRestaurantItem.appendChild(ourRandomRestaurantItemFiller);
+  var ourButton = document.getElementById('generator-button');
+  ourButton.removeChild(button);
+  ourRandomRestaurantItem.classList.add('grow:hover');
+};
+
+button.onclick = myFunction;
